3ff20bb02d26be386a9638a035ec5570eafa8a31
duythai2502 <98018388+duytha2502@users.noreply.github.com>
2025-03-30 03:52:45 +0700

    update fetching data feature and typegen sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
-import StartupCard from '@/components/StartupCard';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
+import { client } from '@/sanity/lib/client';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 export default async function Home({
     searchParams,
 }: {
@@ -7,31 +9,37 @@ export default async function Home({
 }) {
     const query = (await searchParams).query;
 
-    const posts = [{ _createdAt: new Date(), views: 55, author: { _id: 1 } }];
+    const posts = await client.fetch(STARTUPS_QUERY);
+
     return (
         <>
             <section className='pink_container'>
                 <h1 className='heading'>
                     Pitch Your Startup, <br /> Connect With Entrepreneurs
                 </h1>
+
                 <p className='sub-heading !max-w-3xl'>
                     Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
                     Competitions.
                 </p>
 
                 <SearchForm query={query} />
+            </section>
 
-                <section className='section_container'>
-                    <p className='text-30-semibold'>
-                        {query ? `Search results for ${query}` : 'All Startups'}
-                    </p>
+            <section className='section_container'>
+                <p className='text-30-semibold'>
+                    {query ? `Search results for "${query}"` : 'All Startups'}
+                </p>
 
-                    <ul className='mt-7 card_grid'>
-                        {posts.map((post: StartupCardType) => (
+                <ul className='mt-7 card_grid'>
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupCardType) => (
                             <StartupCard key={post._id} post={post} />
-                        ))}
-                    </ul>
-                </section>
+                        ))
+                    ) : (
+                        <p className='no-results'>No startups found</p>
+                    )}
+                </ul>
             </section>
         </>
     );
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -14,6 +14,7 @@ const SearchForm = ({ query }: { query?: string }) => {
 
             <div className='flex gap-2'>
                 {query && <SearchFormReset />}
+
                 <button type='submit' className='search-btn text-white'>
                     <Search className='size-5' />
                 </button>
