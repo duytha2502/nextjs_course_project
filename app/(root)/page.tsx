23b34d6fba0ed2ec2047b76e4ef7880a26c49fb1
duythai2502 <98018388+duytha2502@users.noreply.github.com>
2025-03-29 17:41:36 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,38 @@
+import StartupCard from '@/components/StartupCard';
+import SearchForm from '../../components/SearchForm';
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ query?: string }>;
+}) {
+    const query = (await searchParams).query;
+
+    const posts = [{ _createdAt: new Date(), views: 55, author: { _id: 1 } }];
+    return (
+        <>
+            <section className='pink_container'>
+                <h1 className='heading'>
+                    Pitch Your Startup, <br /> Connect With Entrepreneurs
+                </h1>
+                <p className='sub-heading !max-w-3xl'>
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+                    Competitions.
+                </p>
+
+                <SearchForm query={query} />
+
+                <section className='section_container'>
+                    <p className='text-30-semibold'>
+                        {query ? `Search results for ${query}` : 'All Startups'}
+                    </p>
+
+                    <ul className='mt-7 card_grid'>
+                        {posts.map((post: StartupCardType) => (
+                            <StartupCard key={post._id} post={post} />
+                        ))}
+                    </ul>
+                </section>
+            </section>
+        </>
+    );
+}
