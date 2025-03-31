ada5e80d3524f0f60b0eb10aead1d04febc03cca
duythai2502 <98018388+duytha2502@users.noreply.github.com>
2025-04-01 03:13:55 +0700

    update create startup layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import StartupCard, { StartupCardType } from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 import { sanityFetch, SanityLive } from '@/sanity/lib/live';
+import { auth } from '@/auth';
 export default async function Home({
     searchParams,
 }: {
@@ -13,6 +14,9 @@ export default async function Home({
         query: STARTUPS_QUERY,
         params,
     });
+
+    const session = await auth();
+    console.log(session.id);
 
     return (
         <>
